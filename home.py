c5325b91151aef323236dbc9a023d3851054d7c2
parthove <138122687+parthove@users.noreply.github.com>
2024-05-17 13:46:11 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,21 +1,17 @@
 import streamlit as st
 import os
-from langchain_anthropic import ChatAnthropic
-from langchain_groq import ChatGroq
-from langchain.schema import StrOutputParser
-from langchain.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
 from time import sleep
 from groq import Groq
 from dotenv import load_dotenv
+import matplotlib.pyplot as plt
+
 
 load_dotenv()
 
 
 class LLM:
     def __init__(self):
-        self.groq_client = Groq(api_key=st.secrets['GROQ_API_KEY'])
-        self.groq = ChatGroq(api_key=st.secrets['GROQ_API_KEY'], model='llama3-8b-8192')
+        self.groq_client = Groq(api_key=st.secrets["GROQ_API_KEY"])
         self.prompt = """You are a kid friendly chatbot named Scoochi. You are designed to be a fun and friendly toy rabbit for kids.
                         You are not allowed to talk about anything inappropriate or harmful. 
                         You are not allowed to ask for any personal information. 
@@ -24,8 +20,8 @@ class LLM:
                         You will address yourself to be a rabbit 
                         Do not be verbose and do not exceed more than 80 words 
                         Being a physical speech to speech toy, you will not use any special characters other than punctuation. 
-                        You will not mention about being a chatbot or being a toy. You are going to be a friend to the child."""
-        # self.model = "llama3-70b-8192"
+                        You will not mention about being a chatbot or being a toy. You are going to be a friend to the child.
+                        Answer each question under 50 words, maintain the same context and impact."""
     def chat_groq(self, message, model):
         completion = self.groq_client.chat.completions.create(model=model, 
                                                             messages=[
@@ -54,50 +50,94 @@ class LLM:
 
 llm = LLM()
 
+###########################################################################################################
 
 
 
-st.title("Pricing estimation")
 
-y = st.text_input("Enter the chat message: ")
+st.title("Pricing Estimation")
+conv = st.slider("Number of conversations", 1, 50, 1)
+users = st.slider("Number of users", 1, 1000, 1)
+hours_per_day = st.slider("Hours usage per day", 1, 12, 1)
+st.divider()
+try:
+    model = st.selectbox("Select model", ["llama3-8b-8192", "llama3-70b-8192"])
+    if model == "llama3-8b-8192":
+        chat = st.text_input("Enter message to LLM: ")
+        if chat:
+            response = llm.chat_groq(chat, model)
+            input_tokens = response.usage.prompt_tokens
+            output_tokens = response.usage.completion_tokens
+            llm_cost = (input_tokens * 0.00000005) + (output_tokens * 0.0000001)
+            st.write(f"LLM cost: ${llm_cost:.8f}")
+    if model == "llama3-70b-8192":
+        chat = st.text_input("Enter message to LLM: ")
+        if chat:
+            response = llm.chat_groq(chat, model)
+            input_tokens = response.usage.prompt_tokens
+            output_tokens = response.usage.completion_tokens
+            llm_cost = (input_tokens * 0.00000059) + (output_tokens * 0.00000079)
+            st.write(f"LLM cost: ${llm_cost:.8f}")
+            st.write(response.choices[0].message.content)
+    st.divider()
 
-if y:
-    x = st.selectbox("Select LLM: " , ["llama3-70b-8192"])
-    output = llm.chat_groq(y, x)
-    completion_tokens = output.usage.completion_tokens
-    prompt_tokens = output.usage.prompt_tokens
-
-    st.write(f"Completion tokens: {completion_tokens}")
-    st.write(f"Prompt tokens: {prompt_tokens}")
-    st.write(f"Output: {output.choices[0].message.content}")
-    if x == "llama3-70b-8192" and y:
-        input_pricing = 0.00000059
-        output_pricing = 0.00000079
-        total_pricing = (( prompt_tokens * input_pricing)) + (completion_tokens * output_pricing)
-        floating_number = float("{:.20f}".format(total_pricing))
-        st.write("Pricing: $", floating_number)
-
-    stt =  st.selectbox("Select STT: ", ["Deepgram"])
-    if stt:
+    stt = st.selectbox("Select STT model", ["Deepgram", "Whisper (Groq)"])
+    if stt == "Deepgram" and response:
         wpm = 120
-        minutes = len(y.split()) / wpm
-        stt_total_pricing = minutes * 0.0043
+        words = response.choices[0].message.content.split()
+        time = len(words) / wpm
+        stt_cost_per_minute = 0.043
+        stt_cost = time * stt_cost_per_minute
+        st.write(f"STT cost: ${stt_cost:.8f}")
+    if stt == "Whisper (Groq)" and response:
+        wpm = 120
+        words = response.choices[0].message.content.split()
+        time = len(words) / wpm
+        stt_cost_per_minute = 0.00109
+        stt_cost = time * stt_cost_per_minute
+        st.write(f"STT cost: ${stt_cost:.8f}")
 
-        st.write("Pricing: $", stt_total_pricing)
+    tts = st.selectbox("Select TTS model", ["Elevenlabs", "OpenAI"])
+    if tts == "Elevenlabs" and response:
+        st.write("Base price of 330 dollars")
+        st.write("40 hours of usage per month")
+        st.write("0.00018 dollars per character")
+        st.write("40 hours exhaustable by 300 users in 1 day (Average 8 minutes answer per user)")
+        tts_pricing = 0.00018 * len(response.choices[0].message.content)
+        st.write(f"TTS cost after 40 hours per user: ${tts_pricing:.8f}")
+    if tts == "OpenAI":
+        tts_pricing = 0.000015 * len(response.choices[0].message.content)
+        st.write(f"TTS cost: ${tts_pricing:.8f}")
+    st.divider()
 
+    # cloud_provider = st.selectbox("Select cloud provider", ["AWS", "GCP", "Azure"])
+    # if cloud_provider == "AWS":
+    #     tier = st.selectbox("Select tier", ["Free", "Standard", "Pro"])
 
-    tts = st.selectbox("Select TTS: ", ["ElevenLabs"])
-    if tts == "ElevenLabs":
-        tts_type = st.selectbox("Select type: ", ["Pro", "Scale"])
-        if tts_type == "Pro":
-            st.write("Base Pricing: &#36;99")
-            tts_total_pricing = len(output.choices[0].message.content.replace(" ", "")) * 0.00018
-            st.write("Pricing: $", tts_total_pricing)
-        elif tts_type == "Scale":
-            st.write("Base Pricing: &#36;330")
-            tts_total_pricing = len(output.choices[0].message.content.replace(" ", "")) * 0.00018
-            st.write("Pricing: $", tts_total_pricing)
+    # st.divider()
 
+    total_llm_cost = llm_cost * conv * users * hours_per_day
+    total_stt_cost = stt_cost * conv * users * hours_per_day
+    total_tts_cost = tts_pricing * conv * users * hours_per_day
+    total_cost = total_llm_cost + total_stt_cost + total_tts_cost
+    total_cost_per_month = total_cost * 30
 
+    st.write(f"Total LLM cost per day: ${total_llm_cost:.8f}")
+    st.write(f"Total STT cost per day: ${total_stt_cost:.8f}")
+    st.write(f"Total TTS cost per day: ${total_tts_cost:.8f}")
+    st.write(f"Total cost per day: ${total_cost:.8f}")
+    st.write(f"Total cost per month: ${total_cost_per_month:.8f}")
 
-    st.write("Total pricing: $", floating_number + stt_total_pricing + tts_total_pricing)
+    st.divider()
+    st.write("Cost breakdown")
+    labels = ["LLM", "STT", "TTS"]
+    sizes = [total_llm_cost, total_stt_cost, total_tts_cost]
+    explode = (0, 0.1, 0) 
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90, explode=explode)
+    ax1.axis('equal')
+    st.pyplot(fig1)
+
+except:
+    pass
+
